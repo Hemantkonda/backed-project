ad5ed426f792fb89280e158149df8cf4ff3541a9
Hemant Konda <hemanthkon0591@gmail.com>
2023-07-13 01:55:02 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -2,5 +2,5 @@
 // Creating Database Name and connecting database in local system
 module.exports = {
     DB_NAME : "Idea_App",
-    DB_URL : "mongodb://localhost/idea_app_demo"
+    DB_URL : ""
 }
